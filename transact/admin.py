686e342c8ff43ff06cc98d234f713a4bb5d2c211
waok2 <abachwayne@live.com>
2020-02-26 22:28:58 +0300

    pay and loan models added

--- transact/admin.py
+++ transact/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import loan, pay
 
-# Register your models here.
+
+admin.site.register(loan)
+
+admin.site.register(pay)
--- transact/models.py
+++ transact/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from client.models import client
 
-# Create your models here.
+
+
+class loan(models.Model):
+
+    client_id = models.ForeignKey(client, on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    repay_amount = models.IntegerField()
+    cycles = models.IntegerField()
+    current_amount = models.IntegerField()
+    mode_cycles= models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class pay(models.Model):
+
+    loan_id = models.ForeignKey(loan, on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    cycles = models.IntegerField()
+    mode = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+
